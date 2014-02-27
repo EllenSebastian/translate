af3d7652a84c9f5958202be408a5228096eab49c
Ellen Sebastian <ellens2@stanford.edu>
2014-02-26 16:19:58 -0800

    Update correctionRules.py

--- correctionRules.py
+++ correctionRules.py
@@ -103,34 +103,55 @@ def switchAdjectives (sentence,frenchSentence):
 			sentence[i-1] = temp
 	return string.join(sentence)
 
-# ugh this does not work currently. 
-def removeArticles(sentence):
+def fixPrepositions(sentence):
 	postagged = POStag(sentence)
 	sentence = re.sub("all of","all",sentence)
-	# change "time of more" to "more time"  NOUN of ADJ
-	# change "system of networking" to "networking system" , "market of capital" to "capital market" 
-	# NOUN of NOUN (sometimes) 
-	# change "of vERB" to "to verb"
-	# get rid of WHOSE a 
-	sentence = sentence.split(" ")
-#	pdb.set_trace()
+	sentence = _get_translated_list_without_plural_tags(sentence.split(' '))
+
 	for i in range(0,len(sentence)-2):
 		if (i < len(sentence) -2 and postagged[i][1] in nounTags and sentence[i+1] == "of" and postagged[i+2][1] in adjTags):
-#			print "    switching " + ' '.join(sentence[i:i+3]) + " ->  " + sentence[i+2] + " " + sentence[i]
+			if (printStuff): 
+				print "FIXED PREPOSITION:    switching " + ' '.join(sentence[i:i+3]) + " ->  " + sentence[i+2] + " " + sentence[i]
 			sentence[i+1] = sentence[i]
 			sentence[i] = sentence[i+2]
 			sentence[i+2] = ""
-		if ("DT" in postagged[i][1] and (sentence[i+1] == "the")):
-#			print "     switching " + ' '.join(sentence[i:i+3]) + " -> " + sentence[i] + " " + sentence[i+2]
-			sentence[i+1] = "" 
 		if (sentence[i] == "of" and postagged[i+1][1] in verbTags + adjTags):
-#			print "     switching  of " + sentence[i+1] + " --> to " + sentence[i+1]
+			if (printStuff):	
+				print " FIXED PREPOSITION:     switching  of " + sentence[i+1] + " --> to " + sentence[i+1]
 			sentence[i] = "to"
-		if (sentence[i] in titles and sentence[i+1] == "the"):
+	return ' '.join(sentence)
+
+def removeArticles(sentence):
+	postagged = POStag(sentence)
+	sentence = re.sub("all of","all",sentence)
+	sentence = _get_translated_list_without_plural_tags(sentence.split(' '))
+
+	for i in range(0,len(sentence)-2):
+		if ("DT" in postagged[i][1] and (sentence[i+1] == "the")):
+			if (printStuff): 
+				print "REMOVE ARTICLE:    switching " + ' '.join(sentence[i:i+3]) + " -> " + sentence[i] + " " + sentence[i+2]
+			sentence[i+1] = "" 
+		if (sentence[i] in titles and sentence[i+1] == "the" or sentence[i+1] == "<PLURAL>the</PLURAL>"):
+			if (printStuff):
+				print " REMOVE ARTICLE:  title  "
 			sentence[i+1] = ""
+		if (sentence[i] == "that" and sentence[i+1] == "the"):
+			sentence[i+1] = ""
+			if (printStuff):	
+				print " REMOVE ARTICLE:  " + sentence[i-1] + sentence[i] + sentence[i+1]
 	return string.join(sentence)
 #print POStag(sentence1)
 #switchAdjectives(adjSentence)
+def deBetweenVerbs(sentence, frenchSentence):
+	frenchSentence = frenchSentence.split(" ")
+	postagged = POStag(sentence)
+	sentence = sentence.split(" ")
+	for i in range(0, len(sentence) - 3):
+		if (postagged[i][1] in verbTags and postagged[i + 1][0] == 'de' and postagged[i + 2][1] in verbTags):
+			sentence[i + 1] = "to";
+
+	return ' '.join(sentence)
+
 
 #this method will actually have to check for verb phrases
 def deBetweenVerbs(sentence, frenchSentence):
